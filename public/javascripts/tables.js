6771513a3b48bc9737d977a365a18fdb1294c677
Andrew Jones <ascjones@gmail.com>
2011-09-20 23:08:55 +0200

    got it displaying basic sum via the console

--- public/javascripts/tables.js
+++ public/javascripts/tables.js
@@ -1,4 +1,18 @@
 (function() {
   window.Sum = Backbone.Model.extend({
   });
+
+  window.SumView = Backbone.View.extend({
+    tagName: 'span',
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.template = _.template($('#sum-template').html());
+    },
+
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
 })();
